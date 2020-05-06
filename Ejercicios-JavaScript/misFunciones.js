d2a77d9f0ad977d9a7bace943f99cfd918599ab8
LeoSole423 <leonel.sole423@gmail.com>
2020-05-06 14:19:33 -0300

    Ejercicio Grados Radianes realizado

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -39,3 +39,17 @@ function cambiarunidades(id,valor) {
         document.lasUnidades.unid_pie.value = valor*3;
     }
 }
+
+function convertirgr(id) {
+    var grad, rad;
+    if(id=="grados"){
+        grad=document.getElementById("grados").value;
+        rad=grad*Math.PI/180;
+    }
+    if(id=="radianes"){
+        rad=document.getElementById("radianes").value;
+        grad=rad*180/Math.PI;
+    }
+    document.getElementById("grados").value = grad;
+    document.getElementById("radianes").value = rad;
+}
