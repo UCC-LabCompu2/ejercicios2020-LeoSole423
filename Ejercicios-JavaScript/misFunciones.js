81e18427e3cba9a7e7edad5958e7e3574a88bff2
LeoSole423 <leonel.sole423@gmail.com>
2020-05-13 17:01:13 -0300

    Se realizaron los ejercicios Mostrar-Ocultar y operaciones Matematicas

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -53,3 +53,37 @@ function convertirgr(id) {
     document.getElementById("grados").value = grad;
     document.getElementById("radianes").value = rad;
 }
+
+function mostrar_ocultar(valorMO) {
+    if(valorMO=="val_mostrar"){
+        document.getElementById("divMO").style.display = 'block';
+    }
+    else if(valorMO=="val_ocultar"){
+        document.getElementById("divMO").style.display = 'none';
+    }
+}
+
+function calcular_suma() {
+    var n1,n2;
+    n1=Number(document.getElementsByName("sum_num1")[0].value);
+    n2=document.getElementsByName("sum_num2")[0].value;
+    document.getElementsByName("sum_total")[0].value= n1 + Number(n2);
+}
+function calcular_resta() {
+    var n1,n2;
+    n1=Number(document.getElementsByName("res_num1")[0].value);
+    n2=document.getElementsByName("res_num2")[0].value;
+    document.getElementsByName("res_total")[0].value= n1 - Number(n2);
+}
+function calcular_mul() {
+    var n1,n2;
+    n1=Number(document.getElementsByName("mul_num1")[0].value);
+    n2=document.getElementsByName("mul_num2")[0].value;
+    document.getElementsByName("mul_total")[0].value= n1 * Number(n2);
+}
+function calcular_div() {
+    var n1,n2;
+    n1=Number(document.getElementsByName("div_num1")[0].value);
+    n2=document.getElementsByName("div_num2")[0].value;
+    document.getElementsByName("div_total")[0].value= n1 / Number(n2);
+}
